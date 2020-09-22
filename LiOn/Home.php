9dc3eab6470ec6b3955ee3842d426012947f004e
Ethan_ljayson <lyimoefraim@gmail.com>
2020-09-22 08:53:17 -0700

    new
    New

---
+++ LiOn/Home.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Li'On | ltd</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="Css/lion.css">
+	<link rel="stylesheet" type="text/css" href="Css/team-boxed.css">
+	<link rel="stylesheet" type="text/css" href="Css/user.css">
+	<link rel="stylesheet" type="text/css" href="Css/Testimonials.css">
+	<link rel="stylesheet" type="text/css" href="Css/footer-clean.css">
+
+
+		<!-- CSS only -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- JS, Popper.js, and jQuery -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+	<link rel="stylesheet" type="text/css" href="carousel.css" rel="stylesheet" type="text/css" >
+
+	<style type="text/css">
+	    html, body {
+	      margin: 0;
+	      padding: 0;
+	    }
+
+	</style>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" type="text/javascript" ></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="slick.js" type="text/javascript" charset="utf-8"></script>
+
+</head>
+<body>
+	<section class ="banner">
+		<div class = "slider">
+			<div id="slider" class="carousel slide carousel-fade" data-ride="carousel">
+				<!-- Slider image and caption -->
+			  <div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <img class="d-block w-100" src="Asset/Beautiful.jpg" alt="First slide">
+			      <div class ="carousel-caption">
+			      	<h1>Caption will stay here</h1>
+			      	<p>Live your life me!</p>
+			      </div>
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="Asset/run.jpg" alt="Second slide">
+			      <div class ="carousel-caption">
+			      	<h1>Caption will stay here</h1>
+			      	<p>Live your life me!</p>
+			      </div>
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="Asset/hiphop.jpg" alt="Third slide">
+			      <div class ="carousel-caption">
+			      	<h1>Caption will stay here</h1>
+			      	<p>Live your life me!</p>
+			      </div>
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="Asset/smart boy.jpg" alt="Forth slide">
+			      <div class ="carousel-caption">
+			      	<h1>Caption will stay here</h1>
+			      	<p>Live your life me!</p>
+			      </div>
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="Asset/preety.jpg" alt="Fifth slide">
+			      <div class ="carousel-caption">
+			      	<h1>Caption will stay here</h1>
+			      	<p>Live your life me!</p>
+			      </div>
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="Asset/disappearing.jpg" alt="sixth slide">
+			      <div class ="carousel-caption">
+			      	<h1>Caption will stay here</h1>
+			      	<p>Live your life me!</p>
+			      </div>
+			    </div>
+			  </div>
+			  <!-- Carsoures Indicators -->
+			  <ol class="carousel-indicators">
+			    <li data-target="#slider" data-slide-to="0" class="active"></li>
+			    <li data-target="#slider" data-slide-to="1"></li>
+			    <li data-target="#slider" data-slide-to="2"></li>
+			    <li data-target="#slider" data-slide-to="3"></li>
+			    <li data-target="#slider" data-slide-to="4"></li>
+			    <li data-target="#slider" data-slide-to="5"></li>
+			  </ol>
+			</div>
+		</div>
+
+		<div class="side-menu">
+			<!-- <img src="Asset/menu.png" alt="menu" class="menu"> -->
+		</div>
+
+		<div class="top-bar">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Gallery</a></li>
+				<li><a href="#">Journey</a></li>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Products</a></li>
+			</ul>
+		</div>
+
+		<div class="social-icons">
+			<ul>
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+			</ul>
+		</div>
+	</section>
+
+	<section class="about">
+		<div class="container">
+    		<h1 style="text-align: center; margin: 0px; padding: 0px">Services</h1>
+    		<h4 style="text-align: center;"> We provide the best, Get the test of the future</h4>
+
+    		<div class="row">
+			  <div class="col-sm-6">
+			    <div class="card">
+			      <div class="card-body">
+			        <h5 class="card-title">Designing Solution</h5>
+			        <p class="card-text">Li'On ltd We brand your life to the maximum, We offer branding design. such us Postal, Motion graphics, 2D and 3D Animation</p>
+			        <a href="#" class="btn btn-primary">View us</a>
+			      </div>
+			    </div>
+			  </div>
+			  <div class="col-sm-6">
+			    <div class="card">
+			      <div class="card-body">
+			        <h5 class="card-title">IT services</h5>
+			        <p class="card-text">Services that will help you fill out your customer needs. such us website application, Network system and mobile application</p>
+			        <a href="#" class="btn btn-primary">View Us</a>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+
+			<div class="card text-white">
+			  <img src="Asset/complex perspective.jpg" class="card-img" alt="" >
+			  <div class="card-img-overlay">
+			    <h5 class="card-title">Our Mission</h5>
+			    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			  </div>
+			</div>
+    	 
+		</div>
+	</section>
+
+	<section class="Team">
+		<div class="container">
+    		<h1 style="text-align: center;">Team</h1>
+    		<div class="testimonials-clean">
+		        <div class="row people">
+		            <div class="col-md-4 col-sm-6 item">
+		                <div class="box">
+		                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est.</p>
+		                </div>
+		                <div class="author"><img class="img-circle" src="Asset/Beautiful.jpg" />
+		                    <h5 class="name">Ben Johnson</h5>
+		                    <p class="title">CEO of Company Inc.</p>
+		                </div>
+		            </div>
+		            <div class="col-md-4 col-sm-6 item">
+		                <div class="box">
+		                    <p class="description">Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id.</p>
+		                </div>
+		                <div class="author"><img class="img-circle" src="Asset/Beautiful.jpg" />
+		                    <h5 class="name">Carl Kent</h5>
+		                    <p class="title">Founder of Style Co.</p>
+		                </div>
+		            </div>
+		            <div class="col-md-4 col-sm-6 item">
+		                <div class="box">
+		                    <p class="description">Aliquam varius finibus est, et interdum justo suscipit. Vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu.</p>
+		                </div>
+		                <div class="author"><img class="img-circle" src="Asset/Beautiful.jpg" />
+		                    <h5 class="name">Emily Clark</h5>
+		                    <p class="title">Owner of Creative Ltd.</p>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+    	 
+		</div>
+	</section>
+
+	<section class="Client">
+    		
+		    <div class="container">
+		    	<div class="team-boxed">
+		     	<h1 style="text-align: center;">Client</h1>
+		        <div class="row people">
+		            <div class="col-md-4 col-sm-6 item">
+		                <div class="box"><img class="img-circle" src="Asset/Beautiful.jpg" />
+		                    <h3 class="name">Ethan Ljayson</h3>
+		                    <p class="title">Musician</p>
+		                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.</p>
+		                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+		                </div>
+		            </div>
+		            <div class="col-md-4 col-sm-6 item">
+		                <div class="box"><img class="img-circle" src="Asset/Beautiful.jpg" />
+		                    <h3 class="name">Patrick ly</h3>
+		                    <p class="title">Artist</p>
+		                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.</p>
+		                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+		                </div>
+		            </div>
+		            <div class="col-md-4 col-sm-6 item">
+		                <div class="box"><img class="img-circle" src="Asset/Beautiful.jpg" />
+		                    <h3 class="name">Carl Kent</h3>
+		                    <p class="title">Stylist</p>
+		                    <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.</p>
+		                    <div class="social"><a href="#"><i class="fa fa-facebook-official"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></div>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+		    	 
+		</div>
+	</section>
+
+	<section class="Partners">
+		<div class="container">
+    		<h1 style="text-align: center;">Partners</h1>
+    		<div class="row">
+				<div class="container">
+					<section class="customer-logos slider">
+						<div class="slide"><img src="images/image1.png"></div>
+						<div class="slide"><img src="images/image2.png"></div>
+						<div class="slide"><img src="images/image3.png"></div>
+						<div class="slide"><img src="images/image4.png"></div>
+						<div class="slide"><img src="images/image5.png"></div>
+						<div class="slide"><img src="images/image6.png"></div>
+						<div class="slide"><img src="images/image7.png"></div>
+						<div class="slide"><img src="images/image8.png"></div>
+					</section>
+				</div>
+			</div>
+
+			<script type="text/javascript">
+				$(document).ready(function(){
+					$('.customer-logos').slick({
+						slidesToShow: 6,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 1000,
+						arrows: false,
+						dots: false,
+							pauseOnHover: false,
+							responsive: [{
+							breakpoint: 768,
+							settings: {
+								slidesToShow: 4
+							}
+						}, {
+							breakpoint: 520,
+							settings: {
+								slidesToShow: 3
+							}
+						}]
+					});
+				});
+			</script>
+				
+    	 
+		</div>
+	</section>
+
+	<section class="Footer">
+		<div class="footer-clean">
+	    <footer>
+	        <div class="container">
+	            <div class="row">
+	                <div class="col-md-3 col-sm-4 item">
+	                    <h3>Services</h3>
+	                    <ul>
+	                        <li><a href="#">Web design</a></li>
+	                        <li><a href="#">Development</a></li>
+	                        <li><a href="#">Hosting</a></li>
+	                    </ul>
+	                </div>
+	                <div class="col-md-3 col-sm-4 item">
+	                    <h3>About</h3>
+	                    <ul>
+	                        <li><a href="#">Company</a></li>
+	                        <li><a href="#">Team</a></li>
+	                        <li><a href="#">Legacy</a></li>
+	                    </ul>
+	                </div>
+	                <div class="col-md-3 col-sm-4 item">
+	                    <h3>Careers</h3>
+	                    <ul>
+	                        <li><a href="#">Job openings</a></li>
+	                        <li><a href="#">Employee success</a></li>
+	                        <li><a href="#">Benefits</a></li>
+	                    </ul>
+	                </div>
+	                <div class="col-md-3 item social">
+	                	<div class="contact-clean">
+						    <form method="post">
+						    	<h3 style="text-align: center;">Contact us</h3>
+						        <div class="form-group"><input class="form-control" type="text" name="name" placeholder="Name" /></div>
+						        <div class="form-group"><input class="form-control is-invalid" type="email" name="email" placeholder="Email" /><small class="form-text text-danger">Please enter a correct email address.</small></div>
+						        <div class="form-group"><textarea class="form-control" rows="1" name="message" placeholder="Message"></textarea></div>
+						        <div class="form-group"><button class="btn btn-primary" type="submit">send</button></div>
+						    </form>
+						</div>
+	                </div>
+	                <p class="copyright">Company Name © 2016</p>
+	            </div>
+	        </div>
+	    </footer>
+</div>
+	</section>
+
+
+
+	
+
+</body>
+</html>
